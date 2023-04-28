28edb40c25214e0688eb839e3f1d5594afd34874
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-04-28 17:12:36 +0500

    added javax

--- module-main/src/main/java/com/mjc/school/config/ProjectConfig.java
+++ module-main/src/main/java/com/mjc/school/config/ProjectConfig.java
@@ -4,9 +4,13 @@ package com.mjc.school.config;
 import com.mjc.school.repository.config.RepositoryConfig;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories
+@EnableTransactionManagement
 @EnableAspectJAutoProxy
 @Import({RepositoryConfig.class})
 @ComponentScan(basePackages={"com.mjc.school"})
