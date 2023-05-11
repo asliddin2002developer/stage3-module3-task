8911edef6d5cb9f55a1b8f4327131cc1cc359ec7
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-05-11 12:11:27 +0500

    initial commit

--- module-main/src/main/java/com/mjc/school/main/Main.java
+++ module-main/src/main/java/com/mjc/school/main/Main.java
@@ -5,7 +5,9 @@ import com.mjc.school.config.ProjectConfig;
 import com.mjc.school.controller.menumanager.MenuManager;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class Main {
+
     public static void main(String[] args) {
         try(var context = new AnnotationConfigApplicationContext(ProjectConfig.class)){
             var menuManager = context.getBean(MenuManager.class);
